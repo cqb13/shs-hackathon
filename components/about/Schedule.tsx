9b18662f244f2e616ecaf1cb9fa88e40dde789d5
cqb13 <cqb13.main@gmail.com>
2023-11-24 12:37:47 -0500

    formatting

--- components/about/Schedule.tsx
+++ components/about/Schedule.tsx
@@ -4,12 +4,18 @@ export default function Schedule({ schedule }: { schedule: Event[] }) {
   return (
     <div className='flex flex-col gap-5 w-full'>
       {schedule.map((event, index) => (
-        <div className='flex items-center justify-between gap-10 w-full' key={index}>
-          <h2 className="font-space-mono text-xl text-neutral-700">{event.name}</h2>
-          <h2 className="font-space-mono text-xl text-neutral-700">{event.time}</h2>
+        <div
+          className='flex items-center justify-between gap-10 w-full'
+          key={index}
+        >
+          <h2 className='font-space-mono text-xl text-neutral-700'>
+            {event.name}
+          </h2>
+          <h2 className='font-space-mono text-xl text-neutral-700'>
+            {event.time}
+          </h2>
         </div>
       ))}
-      ;
     </div>
   );
 }

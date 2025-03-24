dbabf41e44bc35661bd2bda2b447d55416efd276
cqb13 <cqb13.main@gmail.com>
2025-03-24 15:41:55 -0400

    added example project

--- app/hackathon/examples/page.tsx
+++ app/hackathon/examples/page.tsx
@@ -2,14 +2,32 @@ export default function Examples() {
   return (
     <main className="flex flex-col gap-10 px-60 py-20 max-lg:px-14 max-sm:px-5 bg-white">
       <section className="grid grid-cols-3 gap-2">
-        <div className="flex flex-col justify-between h-60">
+        <div className="flex flex-col justify-between h-60 rounded-lg bg-onyx p-2 text-white">
           <div>
-            <h2>Urban Climate Resiliency</h2>
-            <p>By: </p>
+            <h2 className="text-xl font-body">Urban Climate Resiliency</h2>
+            <p>By: Apurva Varigonda</p>
           </div>
-          <div>
-            <a href="https://github.com/avarigonda24/Urban-Climate-Resiliency">
+          <div className="flex gap-2 justify-between">
+            <a
+              href="https://github.com/avarigonda24/Urban-Climate-Resiliency"
+              target="_blank"
+              className="text-fairy_tale hover:text-fairy_tale-400 transition-all"
+            >
               View Code
+            </a>
+            <a
+              href="https://docs.google.com/presentation/d/14hc2954oAkexIbY4lKgY1c7SreZ3AaOBwoq10luw06o/edit?usp=sharing"
+              target="_blank"
+              className="text-fairy_tale hover:text-fairy_tale-400 transition-all"
+            >
+              Final Slideshow
+            </a>
+            <a
+              href="https://avarigonda24.github.io/Urban-Climate-Resiliency/index.html"
+              target="_blank"
+              className="text-fairy_tale hover:text-fairy_tale-400 transition-all"
+            >
+              Website
             </a>
           </div>
         </div>
